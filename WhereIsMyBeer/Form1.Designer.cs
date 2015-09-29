3db972ee4630a733ca0f7e17029936d84b119bb3
unknown <todor.yordanov@mail.bg>
2015-09-29 12:46:09 +0300

    TeamWorkProject

---
+++ WhereIsMyBeer/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace WhereIsMyBeer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.screen = new System.Windows.Forms.Panel();
+            this.nakov = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.graund = new System.Windows.Forms.Panel();
+            this.screen.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nakov)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // screen
+            // 
+            this.screen.Controls.Add(this.graund);
+            this.screen.Controls.Add(this.nakov);
+            this.screen.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.screen.Location = new System.Drawing.Point(0, 0);
+            this.screen.Name = "screen";
+            this.screen.Size = new System.Drawing.Size(284, 261);
+            this.screen.TabIndex = 0;
+            // 
+            // nakov
+            // 
+            this.nakov.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.nakov.Location = new System.Drawing.Point(12, 224);
+            this.nakov.Name = "nakov";
+            this.nakov.Size = new System.Drawing.Size(24, 25);
+            this.nakov.TabIndex = 0;
+            this.nakov.TabStop = false;
+            this.nakov.Click += new System.EventHandler(this.nakov_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // graund
+            // 
+            this.graund.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.graund.Location = new System.Drawing.Point(0, 249);
+            this.graund.Name = "graund";
+            this.graund.Size = new System.Drawing.Size(284, 12);
+            this.graund.TabIndex = 1;
+            this.graund.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.screen);
+            this.Name = "Form1";
+            this.Text = "WhereIsMyBeer";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.screen.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.nakov)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel screen;
+        private System.Windows.Forms.PictureBox nakov;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Panel graund;
+    }
+}
+
---
+++ WhereIsMyBeer/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WhereIsMyBeer
+{
+    public partial class Form1 : Form
+    {
+        bool jump;
+        int g = 21;
+        int force;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void nakov_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (jump != true)
+            {
+                if (e.KeyCode == Keys.Space)
+                {
+                    jump = true;
+                    force = g;
+                }
+                if (e.Modifiers == Keys.Shift && e.KeyCode == Keys.Space)
+                {
+                    jump = true;
+                    force = g + 4;
+                }
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (jump == true)
+            {
+                nakov.Top -= force;
+                force -= 1;
+            }
+            if (nakov.Top + nakov.Height + graund.Height >= screen.Height)
+            {
+                nakov.Top = screen.Height - nakov.Height - graund.Height;
+                jump = false;
+            }
+            else
+            {
+                nakov.Top += 5;
+            }
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
